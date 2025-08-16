cc0f6eb5d6d26c46c71a42c51d6ec8513927f489
siliconeisnot <alktm27@gmail.com>
2025-08-16 12:57:00 +0200

    test: add autogluon strategy backtest

---
+++ tests/strategy/strats/FreqaiAutoGluonStrategy.py
@@ -0,0 +1,7 @@
+from tests.strategy.strats.freqai_test_strat import freqai_test_strat
+
+
+class FreqaiAutoGluonStrategy(freqai_test_strat):
+    """Strategy wrapper using AutoGluon prediction model for testing."""
+
+    pass
